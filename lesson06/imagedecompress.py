dcc54863cae3c5d9d4227b7e0b5873c7e06c4981
Gary Godderidge <102984600+ggodderi@users.noreply.github.com>
2025-05-18 14:53:24 -0600

    Added comments and cleaned up the code.

--- lesson06/imagedecompress.py
+++ lesson06/imagedecompress.py
@@ -11,44 +11,66 @@
 import json 
 
 def read_data_from_file(filename):
+    '''Read the data from the file whose name is filename.
+    convert that data from json to a dictionary and return it.'''
     with open(filename, "rt") as filehandle:
         file_data = filehandle.read()
         json_data = json.loads(file_data)
         return json_data
 
 def obtain_file_name():
+    '''Get the compressed image filename from the user.'''
     filename = input('Please input the filename: ')
     return filename
 
 def create_emtpy_image(rows, columns):
+    '''Create an empty image with the given number for rows and columns.'''
     empty_image = []
+
+    # Create a new column with an space at each row.
     for column in range(columns):
         empty_image.append([' ']*rows)
 
     return empty_image
 
 
-def decompress_image(rows, columns, compressed_data, empty_image):
+def decompress_image(columns, compressed_image, decompressed_image):
+    '''Given the columns, compressed_image, and the decompressed image, 
+    Decompress the data into the decompressed_image'''
+
+    # Go through all of the compressed image data for each column
     for column in range(columns):
         # print(compressed_data[column])
-        # output_character = '*'
-        count = 0
+
+        # count is used to switch between On and Off data
+        count = 0  
+
+        # Keep track of which row is currently being decompressed
         row = 0  
-        for d in compressed_data[column]:
+
+        # For each compressed data in the data, decompress it.
+        for d in compressed_image[column]:
             # print(d)          
+
+            # Switch the ON/OFF character based upon whether this is a On or Off bit.
             if not count % 2:
                 output_character = '*'
             else:
                 output_character = ' '
             count += 1
+
+            # Process the number of compressed bits.
             for r in range(d):
                 # print(column, row, output_character)
-                empty_image[column][row] = output_character
+                decompressed_image[column][row] = output_character
                 # print(empty_image[column][row])
                 row += 1
 
 
-def display_image(rows, columns, image_data):
+def display_image(rows, image_data):
+    '''Display the image.  Do so by printing the row entry for each of the columns.  I.e., go 
+    through each row on at a time.'''
+
     for row in range(rows):
         for column in image_data:
             print(column[row], end = ' ')
@@ -63,11 +85,11 @@ def main():
     number_columns = json_data['num_columns']
     image_data = json_data['data']
 
-    empty_image = create_emtpy_image(number_rows, number_columns)
+    decompressed_image = create_emtpy_image(number_rows, number_columns)
     # empty_image[0][1] = 'A'
     # print(empty_image)
 
-    decompress_image(number_rows, number_columns, image_data, empty_image)
+    decompress_image(number_columns, image_data, decompressed_image)
     # print(empty_image)
-    display_image(number_rows, number_columns, empty_image)
+    display_image(number_rows, decompressed_image)
 main()
