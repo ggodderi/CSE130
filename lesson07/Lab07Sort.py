6673ce567d48f012c294945ccea68d90e20c0fef
Gary Godderidge <102984600+ggodderi@users.noreply.github.com>
2026-02-13 10:33:52 -0700

    Updated Lab07 sort to now use for loops.

--- lesson07/Lab07Sort.py
+++ lesson07/Lab07Sort.py
@@ -1,6 +1,7 @@
 # Python
 
 import json
+import random
 
 # Open the given file name and return the data_array
 def read_file(file_name):
@@ -17,7 +18,7 @@ def read_file(file_name):
 # Receive a list of unsorted items and sort them.
 # This uses the "sort-algorithm" from CSE 130, lesson 07
 # Note:  This will work with any sortable data type
-def sort_word_list(data_list):
+def sort_data_list(data_list):
 
     # set pivot and largest at the end of the list
     i_pivot = len(data_list) - 1
@@ -25,10 +26,12 @@ def sort_word_list(data_list):
     counter = 0
 
     # Loop through the entire list
-    while i_pivot >= 0:
+    # while i_pivot >= 0:
+    for i_pivot in range(len(data_list)-1, -1, -1):
 
         # check will start one less than pivot and move to the left
-        for i_check in range(i_pivot-1, -1, -1):
+        # for i_check in range(i_pivot-1, -1, -1):
+        for i_check in range(0, i_pivot, 1):
             counter += 1
             if data_list[i_check] > data_list[i_largest]:
                 i_largest = i_check
@@ -38,15 +41,36 @@ def sort_word_list(data_list):
             (data_list[i_pivot], data_list[i_largest]) = (data_list[i_largest], data_list[i_pivot])
     
         # Move pivot and largest back to the left by 1
-        i_pivot -= 1
-        i_largest = i_pivot
+        # i_pivot -= 1
+        i_largest = i_pivot - 1
 
     print(f'Length: {len(data_list)}, loop counts: {counter}')
+
+
+def create_random_numbers_list(size):
+    random_numbers = []
+    for i in range(size):
+        random_numbers.append(random.randint(-500, 1000))
+    return random_numbers
+
+
+def run_sort_tests(print_output = False):
+    data_size = 2
+
+    while data_size <= 1024:
+        data_set = create_random_numbers_list(data_size)
+        sort_data_list(data_set)
+        data_size *= 2
+        if(print_output):
+            print(data_set)
 
 # Main function for convenience
 def main():
 
-    file_name = input('Please input the name of the file containing the unsorted list of items: ')
+    file_name = input('Please input the name of the file containing the unsorted list of items: (t to run tests) ')
+    if (file_name.lower() == 't'):
+        run_sort_tests(True)
+        exit(1)
     # read the word list
     data_list = read_file(file_name)
 
@@ -54,7 +78,7 @@ def main():
         print(data_list)
 
         # Sort the words and print out the sorted list.
-        sort_word_list(data_list)
+        sort_data_list(data_list)
         print(data_list)
 
 main()
